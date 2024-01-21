193723d8ac854062f5f92556ac8b105eda608976
Juan Dimas Abrante Hernandez <19jdah98@gmail.com>
2024-01-21 00:47:39 +0000

    changes

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /home/pc17-dpl/travelroad_express
+   git add .
+   git commit -m "Changes"
+   git push
+
+ssh dimas@dimas.arkania.es "
+  cd /home/dimas/travelroad_express
+  git pull
+  npm install
+  pm2 restart travelroad --update-env
+"
